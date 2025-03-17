7d517ad79bab673b625a8d40f7a1c93346cfa1d2
Lior Rachmani <liorachmani@gmail.com>
2025-03-17 23:29:46 +0200

    Auth activity

--- app/src/main/java/com/example/skinJourney/AuthenticationActivity.kt
+++ app/src/main/java/com/example/skinJourney/AuthenticationActivity.kt
@@ -1,64 +1,18 @@
-//package com.example.skinJourney
-//
-//import android.os.Bundle
-//import androidx.activity.enableEdgeToEdge
-//import androidx.appcompat.app.AppCompatActivity
-//import androidx.core.view.ViewCompat
-//import androidx.core.view.WindowInsetsCompat
-//import androidx.fragment.app.Fragment
-//import com.example.skinJourney.databinding.ActivityAuthenticationBinding
-//
-//
-//class AuthenticationActivity : AppCompatActivity() {
-//    private lateinit var binding: ActivityAuthenticationBinding  // ViewBinding instance
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        enableEdgeToEdge()
-//
-//        // Inflate the binding for the activity
-//        binding = ActivityAuthenticationBinding.inflate(layoutInflater)
-//        setContentView(binding.root)
-//
-//        // Load LoginFragment initially
-//        if (savedInstanceState == null) {
-//            loadFragment(LoginFragment())
-//        }
-//    }
-//
-//    // Function to load a fragment dynamically
-//    private fun loadFragment(fragment: Fragment) {
-//        supportFragmentManager.beginTransaction()
-//            .replace(R.id.fragmentContainerView, fragment)
-//            .addToBackStack(null) // Enables back navigation
-//            .commit()
-//    }
-//
-//    // Public methods to switch between fragments
-//    fun navigateToRegister() {
-//        loadFragment(RegisterFragment())
-//    }
-//
-//    fun navigateToLogin() {
-//        loadFragment(LoginFragment())
-//    }
-//}
-
-
 package com.example.skinJourney
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.setPadding
-import androidx.fragment.app.Fragment
 import com.example.skinJourney.databinding.ActivityAuthenticationBinding
+import com.example.skinJourney.model.FirebaseModel
 
 class AuthenticationActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAuthenticationBinding
+    private lateinit var firebaseModel: FirebaseModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -70,6 +24,14 @@ class AuthenticationActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        firebaseModel = FirebaseModel()
+
+        if(firebaseModel.get_current_user() != null){
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }
