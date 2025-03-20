953a2ebde5b042538d1c6b347683b8227299dc1b
Ido Winkler <idowin04@gmail.com>
2025-03-20 23:55:51 +0200

    last design

--- app/src/main/java/com/example/skinJourney/MainActivity.kt
+++ app/src/main/java/com/example/skinJourney/MainActivity.kt
@@ -17,7 +17,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
         ProgressBarHandler.init(this)
